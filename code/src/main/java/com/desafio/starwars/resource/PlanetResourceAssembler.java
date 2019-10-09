9fdd5a1ee790427bf39dac7f21e2707200e97e26
Felipe Queres <feliqueres+git@gmail.com>
2019-10-09 03:11:51 -0300

    Retirando generics desnecessários

--- code/src/main/java/com/desafio/starwars/resource/PlanetResourceAssembler.java
+++ code/src/main/java/com/desafio/starwars/resource/PlanetResourceAssembler.java
@@ -13,10 +13,9 @@ import com.desafio.starwars.entity.Planet;
 @Component
 public class PlanetResourceAssembler implements ResourceAssembler<Planet, Resource<Planet>> {
 
-	@Override
-	public Resource<Planet> toResource(final Planet planet) {
-		return new Resource<Planet>(planet,
-				linkTo(methodOn(PlanetController.class).getPlanetById(planet.getId())).withSelfRel());
-	}
-
+    @Override
+    public Resource<Planet> toResource(final Planet planet) {
+        return new Resource<>(planet,
+                linkTo(methodOn(PlanetController.class).getPlanetById(planet.getId())).withSelfRel());
+    }
 }
