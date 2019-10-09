e24d82e752cbbd0ae6261e831866b2ce5c14d503
Felipe Queres <feliqueres+git@gmail.com>
2019-10-09 03:16:54 -0300

    Não exibindo campos nulos ao serializar e construtor vazio

--- code/src/main/java/com/desafio/starwars/entity/dto/PlanetResponse.java
+++ code/src/main/java/com/desafio/starwars/entity/dto/PlanetResponse.java
@@ -1,23 +1,26 @@
 package com.desafio.starwars.entity.dto;
 
+import com.desafio.starwars.entity.Planet;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.util.List;
 
-import com.desafio.starwars.entity.Planet;
-
-import lombok.Data;
-
 @Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class PlanetResponse implements Serializable {
-	private static final long serialVersionUID = -6278930965643900570L;
+    private static final long serialVersionUID = -6278930965643900570L;
 
-	private int size;
-	private String next;
-	private String previous;
-	private List<Planet> planets;
+    private int size;
+    private String next;
+    private String previous;
+    private List<Planet> planets;
 
-	public PlanetResponse(final List<Planet> planets, final int size) {
-		this.planets = planets;
-		this.size = size;
-	}
+    public PlanetResponse(final List<Planet> planets, final int size) {
+        this.planets = planets;
+        this.size = size;
+    }
 }
