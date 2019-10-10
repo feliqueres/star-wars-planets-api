0084d86a055a97076cdefbde85a9edfa68a7b224
Felipe Queres <feliqueres+git@gmail.com>
2019-10-09 23:31:53 -0300

    new constructor

--- code/src/main/java/com/desafio/starwars/entity/Planet.java
+++ code/src/main/java/com/desafio/starwars/entity/Planet.java
@@ -1,14 +1,19 @@
 package com.desafio.starwars.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.validation.constraints.NotBlank;
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "planets")
 @Data
@@ -16,22 +21,30 @@ import java.io.Serializable;
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Planet implements Serializable {
-    private static final long serialVersionUID = 5416080018525612681L;
 
-    @Id
-    private Long id;// TODO auto generate
+	public Planet(final Long id, final String name, final String climate, final String terrain) {
+		this.id = id;
+		this.name = name;
+		this.climate = climate;
+		this.terrain = terrain;
+	}
 
-    @NotBlank
-    @JsonProperty(required = true)
-    private String name;
-    @NotBlank
-    @JsonProperty(required = true)
-    private String climate;
-    @NotBlank
-    @JsonProperty(required = true)
-    private String terrain;
+	private static final long serialVersionUID = 5416080018525612681L;
 
-    @Transient
-    @JsonIgnoreProperties(value = {"amountMoviesAppearance"}, allowGetters = true)
-    private int amountMoviesAppearance;
+	@Id
+	private Long id;// TODO auto generate
+
+	@NotBlank
+	@JsonProperty(required = true)
+	private String name;
+	@NotBlank
+	@JsonProperty(required = true)
+	private String climate;
+	@NotBlank
+	@JsonProperty(required = true)
+	private String terrain;
+
+	@Transient
+	@JsonIgnoreProperties(value = { "amountMoviesAppearance" }, allowGetters = true)
+	private int amountMoviesAppearance;
 }
